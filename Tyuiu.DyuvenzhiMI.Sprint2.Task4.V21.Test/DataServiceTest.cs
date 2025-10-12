2175127bf916d432fd35f02b5112bd12cc743e1e
DyuvenzhiMaxim <Tyuiu.DyuvenzhiMI@outlook.com>
2025-10-12 21:07:55 +0500

    Task4

---
+++ Tyuiu.DyuvenzhiMI.Sprint2.Task4.V21.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿
+using Tyuiu.DyuvenzhiMI.Sprint2.Task4.V21.Lib;
+
+namespace Tyuiu.DyuvenzhiMI.Sprint2.Task4.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            double x = 1; double y = 1;
+            DataService ds = new DataService();
+            double res = ds.Calculate(x, y);
+            Assert.AreEqual(0.905, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            double x = 1; double y = 4;
+            DataService ds = new DataService();
+            double res = ds.Calculate(x, y);
+            Assert.AreEqual(256, res);
+        }
+    }
+}
